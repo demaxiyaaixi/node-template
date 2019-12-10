9f60c8f17b221f81ccba61a47a4fb4f9f8e8d9ac
fangjin <974702892@qq.com>
2019-12-10 13:53:26 +0800

    test demo 1

---
+++ myapp/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//设置路由
+app.get('/', function (req, res) {
+  res.send('Hello World??!');
+});
+
+//设置端口
+const server = app.listen(3001, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Example app listening at http://', host, port);
+});
